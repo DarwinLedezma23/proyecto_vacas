93e855e1cf7a74af349d95879c008d228a8388bb
DarwinLedezma23 <Darwinledezma23@gmail.com>
2025-11-11 15:40:33 -0400

    Manejo de la base de datos

---
+++ database.py
@@ -0,0 +1,29 @@
+import sqlite3 
+
+def crear_tabla():
+    conn = sqlite3.connect("vacas.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS vacas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        fecha_inseminacion TEXT,
+        cantidad_partos INTEGER,
+        fecha_posible_parto TEXT
+        fecha_secado TEXT,
+        concepcion exitosa INTEGER
+        observaciones TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+    def insertar_vaca(nombre, fecha_inseminacion, cantidad_partos, fecha_posible_parto, fecha_secado, concepcion_exitosa, observaciones):
+        conn = sqlite3.connect("vacas.db")
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO vacas (nombre, fecha_inseminacion, cantidad_partos, fecha_posible_parto, fecha_secado, concepcion_exitosa, observaciones)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (nombre, fecha_inseminacion, cantidad_partos, fecha_posible_parto, fecha_secado, concepcion_exitosa, observaciones))
+        conn.commit()
+        conn.close()
